d771d9cfa3466b18bfe5ade368888eb611a5aee0
Thomas Morris <tmorris@salsitasoft.com>
2019-11-26 00:39:25 +0100

    add seeds for lines 1 thru 12

---
+++ lib/line_creators.rb
@@ -0,0 +1,9 @@
+def create_line(line = [])
+  line.each_with_index do |station_name, index|
+    station = Station.find_or_create_by(name: station_name)
+    if index > 0
+      previous_station = Station.find_or_create_by(name: line[index - 1])
+      station.add_neighbor(previous_station)
+    end
+  end
+end
